4f36ae45bbdb018eeebe3f0af7350d6eeb1fb017
Sean Jun <seanjun21@gmail.com>
2016-11-23 00:53:24 -0700

    statements whitespaces and indenting

---
+++ StatementsWhiteSpaceAndIndenting/src/com/seanjun/Main.java
@@ -0,0 +1,23 @@
+package com.seanjun;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
+    }
+}
