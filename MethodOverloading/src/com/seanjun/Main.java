8829d8aef4882972c2e2786d60d65240fb1c4f87
Sean Jun <seanjun21@gmail.com>
2016-11-25 00:06:34 -0700

    method overloading-2

--- MethodOverloading/src/com/seanjun/Main.java
+++ MethodOverloading/src/com/seanjun/Main.java
@@ -3,10 +3,16 @@ package com.seanjun;
 public class Main {
 
     public static void main(String[] args) {
-	    int newScore = calculateScore("Tim", 500);
+        int newScore = calculateScore("Tim", 500);
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        double centimeters = calcFeetAndInchesToCentimeters(6, 0);
+        if (centimeters < 0.0) {
+            System.out.println("Invalid parameters");
+        }
+
+        calcFeetAndInchesToCentimeters(157);
 
         // Create a method called calcFeetAndInchesToCentimeters
         // It needs to have two parameters.
@@ -33,9 +39,31 @@ public class Main {
         // use the link I give you to confirm your code is calculating correctly.
         // Calling another overloaded method just requires you to use the
         // right number of parameters.
-
-
     }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if ((feet < 0) || (inches < 0) || (inches > 12)) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            return -1;
+        }
+
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points ");
