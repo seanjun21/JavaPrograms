87f67928981c958e4dfac9bf9e9c3692ff76b57d
Sean Jun <seanjun21@gmail.com>
2016-11-26 01:56:35 -0700

    classes

---
+++ Classes/src/com/seanjun/Car.java
@@ -0,0 +1,13 @@
+package com.seanjun;
+
+/**
+ * Created by seanjun on 11/26/16.
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+}
