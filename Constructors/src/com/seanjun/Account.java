5ec7c9a8b6e9ab3855e31b5b32c79dea6fd9f644
Sean Jun <seanjun21@gmail.com>
2016-11-26 23:26:10 -0700

    update

--- Constructors/src/com/seanjun/Account.java
+++ Constructors/src/com/seanjun/Account.java
@@ -24,13 +24,17 @@ public class Account {
         this.customerPhoneNumber = customerPhoneNumber;
     }
 
+    public Account(String customerName, String customerEmailAddress, String customerPhoneNumber) {
+        this("99999", 100.55, customerName, customerEmailAddress, customerPhoneNumber);
+    }
+
     public void deposit(double depositAmount) {
         this.balance += depositAmount;
         System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
     }
 
     public void withdrawal(double withdrawalAmount) {
-        if (balance - withdrawalAmount <= 0) {
+        if (balance - withdrawalAmount < 0) {
             System.out.println("Only " + balance + " available. Withdrawal not processed");
         } else {
             this.balance -= withdrawalAmount;
