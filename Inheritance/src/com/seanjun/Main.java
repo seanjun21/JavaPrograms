b4c7fc24615696156891c0eae426ad87b5212583
Sean Jun <seanjun21@gmail.com>
2016-11-28 01:29:49 -0700

    inheritance

--- Inheritance/src/com/seanjun/Main.java
+++ Inheritance/src/com/seanjun/Main.java
@@ -6,6 +6,8 @@ public class Main {
         Animal animal = new Animal("Animal", 1, 1, 5, 5);
 
         Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
-        dog.eat();
+//        dog.eat();
+        dog.walk();
+//        dog.run();
     }
 }
