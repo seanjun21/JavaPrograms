769739c54852cf1b0d1712763eae2d6eb248ae8b
Sean Jun <seanjun21@gmail.com>
2016-12-06 15:00:06 -0700

    composition

---
+++ Composition/src/com/seanjun/Vehicle.java
@@ -0,0 +1,12 @@
+package com.seanjun;
+
+/**
+ * Created by seanjun on 12/6/16.
+ */
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
