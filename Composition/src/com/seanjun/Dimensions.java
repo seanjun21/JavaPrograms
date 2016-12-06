98f2ea21caec3434176027f85ec1441d69437ef9
Sean Jun <seanjun21@gmail.com>
2016-12-06 15:33:42 -0700

    Composition again

---
+++ Composition/src/com/seanjun/Dimensions.java
@@ -0,0 +1,28 @@
+package com.seanjun;
+
+/**
+ * Created by seanjun on 12/6/16.
+ */
+public class Dimensions {
+    private int width;
+    private int height;
+    private int depth;
+
+    public Dimensions(int width, int height, int depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+}
