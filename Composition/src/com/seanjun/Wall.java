c23d6a1f94a46fb7b5f7507c8810f3be47387b19
Sean Jun <seanjun21@gmail.com>
2016-12-08 00:40:48 -0700

    compositions

---
+++ Composition/src/com/seanjun/Wall.java
@@ -0,0 +1,16 @@
+package com.seanjun;
+
+/**
+ * Created by seanjun on 12/7/16.
+ */
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
