c8d170607a25bb8377ac3ad2fd8d29f4acc9f915
Sean Jun <seanjun21@gmail.com>
2016-12-22 21:54:50 -0700

    encapsulation

---
+++ Encapsulation/src/com/seanjun/Player.java
@@ -0,0 +1,23 @@
+package com.seanjun;
+
+/**
+ * Created by seanjun on 12/16/16.
+ */
+public class Player {
+
+    public String name;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+        if (this.health <= 0) {
+            System.out.println("Player knocked out");
+            // Reduce number of lives remaining for the player
+        }
+    }
+
+    public int healthRemaining() {
+        
+    }
+}
