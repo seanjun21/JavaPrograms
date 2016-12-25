b7b3dd5ee642809524eb96df51f5d3ed9e5ca425
Sean Jun <seanjun21@gmail.com>
2016-12-25 03:07:17 -0700

    polymorphism challenges3

--- PolymorphismChallenge/src/com/seanjun/Main.java
+++ PolymorphismChallenge/src/com/seanjun/Main.java
@@ -1,8 +1,56 @@
 package com.seanjun;
 
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // We are going to go back to the car analogy.
+        // Create a base class called Car
+        // It should have a few fields that would be appropriate for a generic car class.
+        // engine, cylinders, wheels, etc.
+        // Constructor should initialize cylinders (number of) and name, and set wheels to 4
+        // and engine to true. Cylinders and names would be passed parameters.
+        //
+        // Create appropriate getters
+        //
+        // Create some methods like startEngine, accelerate, and brake
+        //
+        // show a message for each in the base class
+        // Now create 3 sub classes for your favorite vehicles.
+        // Override the appropriate methods to demonstrate polymorphism in use.
+        // put all classes in the one java file (this one).
     }
 }
