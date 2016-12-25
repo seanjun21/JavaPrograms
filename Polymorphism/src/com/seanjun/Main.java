d9e0b5b69cbdc98270caf0a7026656001afe0232
Sean Jun <seanjun21@gmail.com>
2016-12-25 01:51:12 -0700

    polymorphism2

--- Polymorphism/src/com/seanjun/Main.java
+++ Polymorphism/src/com/seanjun/Main.java
@@ -10,11 +10,93 @@ class Movie {
     public String plot() {
         return "No plot here";
     }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lot of people";
+    }
+
+}
+
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet Earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze";
+    }
+}
+
+class Starwars extends Movie {
+    public Starwars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperials forces try to take over the universe";
+    }
+}
+
+class Forgettable extends Movie {
+    public Forgettable() {
+        super("Forgettable");
+    }
+
+    // No plot method
 }
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 1; i < 11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i + " : " + movie.getName() + "\n" + "Plot: " + movie.plot() + "\n");
+        }
+    }
+
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was: " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new IndependenceDay();
+
+            case 3:
+                return new MazeRunner();
+
+            case 4:
+                return new Starwars();
+
+            case 5:
+                return new Forgettable();
+        }
+
+        return null;
     }
 }
