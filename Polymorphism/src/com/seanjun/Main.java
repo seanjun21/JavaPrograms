d359839871dfd1f4292ef3b33259104a23a25e98
Sean Jun <seanjun21@gmail.com>
2016-12-24 23:45:55 -0700

    polymorphism

---
+++ Polymorphism/src/com/seanjun/Main.java
@@ -0,0 +1,20 @@
+package com.seanjun;
+
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here";
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
