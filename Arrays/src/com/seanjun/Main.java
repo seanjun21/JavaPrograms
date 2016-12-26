cef79a3da6507da4f92db5f7b6a7ec67e540088e
Sean Jun <seanjun21@gmail.com>
2016-12-25 22:41:11 -0700

    arrays3

--- Arrays/src/com/seanjun/Main.java
+++ Arrays/src/com/seanjun/Main.java
@@ -3,7 +3,18 @@ package com.seanjun;
 public class Main {
 
     public static void main(String[] args) {
-        int[] myIntArray = new int[10];
-        myIntArray[5] = 50;
+        int[] myIntArray = new int[10]; // {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+//        myIntArray[0] = 45;
+//        myIntArray[1] = 476;
+//        myIntArray[5] = 50;
+
+        for (int i = 0; i < 10; i++) {
+            myIntArray[i] = i * 10;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Element " + i + " value is " + myIntArray[i]);
+        }
+
     }
 }
