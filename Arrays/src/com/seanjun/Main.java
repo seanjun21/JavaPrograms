1030a25fb87c9936416aec4cfb460199f303a243
Sean Jun <seanjun21@gmail.com>
2016-12-25 20:56:00 -0700

    arrays2

--- Arrays/src/com/seanjun/Main.java
+++ Arrays/src/com/seanjun/Main.java
@@ -3,6 +3,7 @@ package com.seanjun;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
     }
 }
